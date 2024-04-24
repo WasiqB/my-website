0f7a4aa522216da68557cfd7637b8a7f1a43603b
WasiqB <wasbhamla2005@gmail.com>
2024-04-24 15:42:03 +0530

    feat: :sparkles: added create course ui

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,7 +1,104 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import axios, { AxiosError } from "axios";
+import { useToast } from "@/components/ui/use-toast";
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required.",
+  }),
+});
 const CreatePage = () => {
-  return <div>Create Course!</div>;
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+  const { isSubmitting, isValid } = form.formState;
+  const { toast } = useToast();
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/course", values);
+      router.push(`/teacher/courses/${response.data.id}`);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast({
+          variant: "destructive",
+          title: "Error Occurred!",
+          description: error.message,
+          duration: 5000,
+        });
+      }
+    }
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div>
+        <h1 className="text-2xl">Name your Course</h1>
+        <p className="text-sm text-slate-600">
+          What would you like to name your Course? Don&apos;t worry, you can
+          change this later.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="course-title"
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Software Testing Course'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will you teach in this course?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button type="button" variant="ghost">
+                  Cancel
+                </Button>
+              </Link>
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePage;
