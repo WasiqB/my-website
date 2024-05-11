9cb3673ae8d35e9d077cf1f69093d665dcd82416
WasiqB <wasbhamla2005@gmail.com>
2024-05-11 14:32:48 +0530

    feat: :sparkles: added db schema

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -36,7 +36,7 @@ const CreatePage = () => {
   const { toast } = useToast();
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const response = await axios.post("/api/course", values);
+      const response = await axios.post("/api/courses", values);
       router.push(`/teacher/courses/${response.data.id}`);
     } catch (error) {
       if (error instanceof AxiosError) {
